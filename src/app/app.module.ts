87585fab2c52aee5a5ad55d552c0186fb3b865cf
Ryan Taylor <RyanCTaylor95@gmail.com>
2019-12-19 15:58:56 +1000

    Removed angular landing, Routing between 2 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './components/landing/landing.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
+const appRoutes: Routes = [
+  {path: 'landing', component: LandingComponent},
+  {path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
+    //other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
